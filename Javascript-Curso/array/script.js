e6860debbac28b31c7688de98025df29b577e06d
Samuel Bonini <109481774+samuelbonini@users.noreply.github.com>
2024-06-28 23:06:25 -0300

    att

---
+++ Javascript-Curso/array/script.js
@@ -0,0 +1,29 @@
+
+
+var i = 0;
+while (i < 1) {
+  console.log(i);
+  i++;
+}
+
+
+var videoGames = ['Switch', 'ps4', 'xbox']; {
+for (var item = 0; item < videoGames.length; item++)
+  console.log(videoGames[item]) }
+
+
+var copa = [1959, 1962, 1970, 1994, 2002]; {
+  console.log('O brasil ganhou a copa de', copa)
+}
+
+
+
+var frutas = ['Banana', 'Maçã', 'Pera', 'Uva', 'Melância'] 
+  for (var fruta =  0; fruta < frutas.length; fruta++) {
+    console.log(frutas[fruta]);
+  if(frutas[fruta] === 'Pera') {
+    break;
+  }
+}
+
+var ultimaFruta = frutas[frutas.length - 1];
---
+++ Javascript-Curso/atribuicao-e-ternario]/script.js
@@ -0,0 +1,9 @@
+var numero = 20;
+
+numero += numero + 10;
+console.log(numero);
+
+var idade = 20;
+
+var podeBeber;
+podeBeber = (idade >= 18 && naoPossuiDiabetes) ?
