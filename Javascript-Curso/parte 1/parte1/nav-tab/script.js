c0aa7d33c2941044eec029aaee3ea1b604e51d6b
Samuel Bonini <109481774+samuelbonini@users.noreply.github.com>
2024-07-29 21:20:08 -0300

    update

--- Javascript-Curso/parte 1/parte1/nav-tab/script.js
+++ Javascript-Curso/parte 1/parte1/nav-tab/script.js
@@ -1,9 +1,20 @@
-const tabMenu = document.querySelectorAll('.js-tabmenu li');
-const tabContent = document.querySelectorAll('.tabcontent section');
+const tabMenu = document.querySelectorAll(".js-tabmenu li");
+const tabContent = document.querySelectorAll(".js-tabcontent section");
+tabContent[0].classList.add("ativo");
 
-function activeTab(index) {
-  tabContent.forEach((section) => {
-    section.classList.remove('ativo');
-  })
-  tabContent[index].classList.add('ativo');
+if (tabMenu.length && tabContent.length) {
+  tabContent[0].classList.add("ativo");
+
+  function activeTab(index) {
+    tabContent.forEach((section) => {
+      section.classList.remove("ativo");
+    });
+    tabContent[index].classList.add("ativo");
+  }
+
+  tabMenu.forEach((itemMenu, index) => {
+    itemMenu.addEventListener("click", () => {
+      activeTab(index);
+    });
+  });
 }
