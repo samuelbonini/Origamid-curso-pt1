d4511595783a6ca5b9bfafa13b921039fe87d209
Samuel Bonini <109481774+samuelbonini@users.noreply.github.com>
2024-10-17 22:02:27 -0300

    att

---
+++ Javascript-Curso/parte 1/parte1/constructor function/aula 2.js
@@ -0,0 +1,7 @@
+const Dom = {
+  seletor: 'li',
+  element() {
+const elementoSelecionado = document.querySelector(this.seletor);
+return elementoSelecionado;
+  }
+}
---
+++ Javascript-Curso/parte 1/parte1/constructor function/script.js
@@ -0,0 +1,12 @@
+function Carro (marcaAtribuida, precoAtribuido) {
+  this.marca = marcaAtribuida;
+  this.preco = precoAtribuido;
+}
+
+const honda = new Carro ('Honda', 3000);
+const fiat = new Carro ('Fiat', 4000);
+
+function Carro2(marca, precoInicial) {
+  this.marca = marcaAtribuida;
+  this.preco = precoAtribuido;
+}
