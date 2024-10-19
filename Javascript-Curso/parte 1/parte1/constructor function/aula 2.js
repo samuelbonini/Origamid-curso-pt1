08246e59d7dafcdc9e0ea3142cbe501cbec2ebf8
Samuel Bonini <109481774+samuelbonini@users.noreply.github.com>
2024-10-19 15:59:11 -0300

    att

--- Javascript-Curso/parte 1/parte1/constructor function/aula 2.js
+++ Javascript-Curso/parte 1/parte1/constructor function/aula 2.js
@@ -1,7 +1,26 @@
-const Dom = {
-  seletor: 'li',
-  element() {
-const elementoSelecionado = document.querySelector(this.seletor);
-return elementoSelecionado;
+// const Dom = {
+//   seletor: 'li',
+//   element() {
+// return elementoSelecionado = document.querySelector(this.seletor);
+//   },
+//   ativar() {
+// this.element().classList.add('ativar');
+
+//   }
+// }
+
+// Dom.ativar();
+// Dom.seletor = 'ul';
+// Dom.ativar();
+
+function Dom(seletor) {
+  this.element = function() {
+    return document.querySelector(seletor);
+  }
+  this.ativar = function(classe) {
+    this.element().classList.add(classe);
   }
 }
+
+const li = new Dom('li');
+const ul = new Dom('ul');
