c76023163c88b1298b84b6dcbd401d8cfaa706b7
Samuel Bonini <109481774+samuelbonini@users.noreply.github.com>
2024-12-29 20:34:04 -0300

    Update script.js

--- Javascript-Curso/parte 1/parte1/array e iteração/script.js
+++ Javascript-Curso/parte 1/parte1/array e iteração/script.js
@@ -7,11 +7,16 @@
 
 // console.log(carros);
   
-const carros = ['ford', 'fiat', 'honda'];
+const carros = ['ferrari', 'fusca', 'honda'];
 
 const novaArray = carros.map((item, index, array) => {
-  console.log(item.toUpperCase(), index, array);
-  return;
+  console.log(item, index, array);
+  return item;
 })
 
 console.log(novaArray);
+
+const numeros = [1, 4, 8, 12, 28]
+const numeros2 = numeros.map(n => n * 2);
+
+console.log(numeros2);
