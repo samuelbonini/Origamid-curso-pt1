7061a6a2231cf153f2f8926ab919f4f1973818b5
Samuel Bonini <109481774+samuelbonini@users.noreply.github.com>
2025-02-11 22:15:50 -0300

    astt

---
+++ Javascript-Curso/parte 2 dom/scroll-link/animacoes.js
@@ -0,0 +1,80 @@
+function initTabNav() {
+  const tabMenu = document.querySelectorAll("[data-tab="menu"] li");
+  const tabContent = document.querySelectorAll("[data-tab="content"] section");
+  tabContent[0].classList.add("ativo");
+
+  if (tabMenu.length && tabContent.length) {
+    tabContent[0].classList.add("ativo");
+
+    function activeTab(index) {
+      tabContent.forEach((section) => {
+        section.classList.remove("ativo");
+      });
+      const direcao = tabContent[index].dataset.anime;
+      tabContent[index].classList.add("ativo");
+    }
+
+    tabMenu.forEach((itemMenu, index) => {
+      itemMenu.addEventListener("click", () => {
+        activeTab(index);
+      });
+    });
+  }
+}
+initTabNav();
+
+
+function initAccordion() {
+
+const accordionList = document.querySelectorAll(".js-accordion dt");
+const activeClass = 'ativo';
+
+if(accordionList.length) {
+accordionList[0].classList.add("ativo");
+accordionList[0].nextElementSibling.classList.add("ativo");
+
+function activeAccordion() {
+  this.classList.toggle("ativo");
+  this.nextElementSibling.classList.toggle("ativo");
+}
+accordionList.forEach((item) => {
+  item.addEventListener("click", activeAccordion);
+});
+}
+}
+initAccordion();
+
+
+
+
+function initScrollSuave(event) {
+const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]');
+
+function scrollToSection(event) {
+  event.preventDefault();
+  const href = event.currentTarget.getAttribute('href');
+  const section = document.querySelector(href);
+
+section.scrollIntoView({
+  behavior: 'smooth',
+  block: 'start',
+})
+
+
+
+/*FORMA ALTERNATIVA
+const topo = section.offsetTop;
+window.scrollTo ({
+  top: topo,
+  behavior: 'smooth',
+})
+FORMA ALTERNATIVA*/
+
+}
+
+linksInternos.forEach((link) => {
+  link.addEventListener('click', scrollToSection)
+});
+}
+
+initScrollSuave();
