478b9be80946a2f36f9cf9f9aecb91daabae028b
Samuel Bonini <109481774+samuelbonini@users.noreply.github.com>
2025-02-24 20:38:56 -0300

    att

--- Javascript-Curso/parte 2 dom/nav-tab copy/js/modules/animacoes.js
+++ Javascript-Curso/parte 2 dom/nav-tab copy/js/modules/animacoes.js
@@ -2,42 +2,22 @@ function initTabNav() {
   const tabMenu = document.querySelectorAll('[data-tab="menu"] li');
   const tabContent = document.querySelectorAll('[data-tab="content"] section');
 
-  if(tabMenu.length && tabContent.length) {
-    tabContent[0].classList.add('ativo');
+  if (tabMenu.length && tabContent.length) {
+    tabContent[0].classList.add("ativo");
 
     function activeTab(index) {
       tabContent.forEach((section) => {
-        section.classList.remove('ativo');
+        section.classList.remove("ativo");
       });
       const direcao = tabContent[index].dataset.anime;
-      tabContent[index].classList.add('ativo', direcao);
+      tabContent[index].classList.add("ativo", direcao);
     }
 
     tabMenu.forEach((itemMenu, index) => {
-      itemMenu.addEventListener('click', () => {
+      itemMenu.addEventListener("click", () => {
         activeTab(index);
       });
     });
   }
 }
 initTabNav();
-
-function initAccordion() {
-  const accordionList = document.querySelectorAll('[data-anime="accordion"] dt');
-  const activeClass = 'ativo';
-  
-  if(accordionList.length) {
-    accordionList[0].classList.add(activeClass);
-    accordionList[0].nextElementSibling.classList.add(activeClass);
-
-    function activeAccordion() {
-      this.classList.toggle(activeClass);
-      this.nextElementSibling.classList.toggle(activeClass);
-    }
-
-    accordionList.forEach((item) => {
-      item.addEventListener('click', activeAccordion);
-    });
-  }
-}
-initAccordion();
--- Javascript-Curso/parte 2 dom/nav-tab copy/js/script.js
+++ Javascript-Curso/parte 2 dom/nav-tab copy/js/script.js
@@ -1,6 +1,7 @@
-import initScrollSuave from "./modules/scroll-suave-ujs";
+import initScrollSuave from "./modules/scroll-suave.js";
 import initAnimacaoScroll from "./modules/scroll-animacao.js";
-
+import initAnimacaoScroll from "./modules/accordion.js";
 
 initScrollSuave();
 initAnimacaoScroll();
+initAccordion();
