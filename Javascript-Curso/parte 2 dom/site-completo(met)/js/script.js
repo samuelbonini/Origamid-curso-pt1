522949db23f6cd13c196ce34a9b912e4f2d1b9db
Samuel Bonini <109481774+samuelbonini@users.noreply.github.com>
2025-03-03 22:43:28 -0300

    att

--- Javascript-Curso/parte 2 dom/site-completo(met)/js/script.js
+++ Javascript-Curso/parte 2 dom/site-completo(met)/js/script.js
@@ -3,9 +3,11 @@ import initAnimacaoScroll from "./modules/scroll-animacao.js";
 import initAccordion from "./modules/accordion.js";
 import initTabnav from "./modules/tabnav.js";
 import initModal from "./modules/modal.js";
+import initTooltip from "./modules/tooltip.js";
 
 initScrollSuave();
 initAnimacaoScroll();
 initAccordion();
 initTabnav();
 initModal();
+initTooltip();
