265b390833892d3a91ea23d08f38f09a4f0244be
Samuel Bonini <109481774+samuelbonini@users.noreply.github.com>
2025-03-06 22:18:35 -0300

    att

--- Javascript-Curso/parte 2 dom/site2-completo-pt2-dropdown-menu/js/script.js
+++ Javascript-Curso/parte 2 dom/site2-completo-pt2-dropdown-menu/js/script.js
@@ -4,7 +4,7 @@ import initAccordion from "./modules/accordion.js";
 import initTabnav from "./modules/tabnav.js";
 import initModal from "./modules/modal.js";
 import initTooltip from "./modules/tooltip.js";
-import initDropdownMenu from './modules/dropdown-menu.js';
+import initDropdownMenu from "./modules/dropdown-menu.js";
 
 initScrollSuave();
 initAnimacaoScroll();
