844a7b1cb11e198d31d49fdd99a82601082432dd
Samuel Bonini <109481774+samuelbonini@users.noreply.github.com>
2025-04-08 22:21:16 -0300

    att

--- Javascript-Curso/JS-Assincrono/fetch/script.js
+++ Javascript-Curso/JS-Assincrono/fetch/script.js
@@ -1,12 +1,62 @@
-const doc = fetch('./style.css');
+// Utilizando a API https://viacep.com.br/ws/${CEP}/json/
+// crie um formulário onde o usuário pode digitar o cep
+const inputCep = document.getElementById("cep");
+const btnCep = document.getElementById("btnCep");
+const resultadoCep = document.querySelector("resultadoCep");
 
-doc.then(resolucao => {
-  return resolucao.text();
-}).then((body) => {
-  const conteudo = document.querySelector('.conteudo');
-const style = document.createElement('style');
-style.innerHTML = body;
-console.log(style);
+btnCep.addEventListener("click", handleClick);
 
-conteudo.innerText = body;
-})
+function handleClick(event) {
+  event.preventDefault();
+  const cep = inputCep.value;
+  buscaCep(cep);
+}
+
+function buscaCep(cep) {
+  fetch(`https://viacep.com.br/ws/${cep}/json/`)
+    .then((response) => response.text())
+    .then((body) => {
+      resultadoCep.innerText = body;
+    });
+}
+
+// e o endereço completo é retornado ao clicar em buscar
+
+// Utilizando a API https://blockchain.info/ticker
+// retorne no DOM o valor de compra da bitcoin and reais.
+// atualize este valor a cada 30s
+
+const btcDisplay = document.querySelector(".btc");
+
+function fetchBtc() {
+  fetch("https://blockchain.info/ticker")
+    .then((response) => response.json())
+    .then((btcJson) => {
+      btcDisplay.innerText = "💰" + btcJson.USD.buy;
+      // btcDisplay.innerText = ('R$' + btcJson.BRL.buy).replace('.', ',');
+
+    })
+}
+
+setInterval(fetchBtc, 500);
+
+fetchBtc();
+
+// Utilizando a API https://api.chucknorris.io/jokes/random
+// retorne uma piada randomica do chucknorris, toda vez que
+// clicar em próxima
+
+const btnProxima = document.querySelector('.proxima');
+const paragrafoPiada = document.querySelector('.piada');
+
+function puxarPiada() {
+  fetch('https://api.chucknorris.io/jokes/random')
+  .then (r => r.json())
+  .then (piada => {
+    paragrafoPiada.innerText = piada.value;
+  })
+}
+
+btnProxima.addEventListener('click', puxarPiada);
+
+puxarPiada();
