081990f20bdacef17c6ed5ef012bbee93aa55c42
Samuel Bonini <109481774+samuelbonini@users.noreply.github.com>
2025-05-06 22:07:59 -0300

    att

--- Javascript-Curso/classes/loops-iterable/script.js
+++ Javascript-Curso/classes/loops-iterable/script.js
@@ -4,3 +4,9 @@ const frase = 'Isso é JavaScript';
 fetch('https://pokeapi.co/api/v2/pokemon/')
 .then(({headers}) => console.log(headers));
  
+
+
+const itens = document.querySelectorAll('li');
+for (const item of itens) {
+  item.classList.add('ativo');
+}
