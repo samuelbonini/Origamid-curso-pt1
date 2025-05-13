dff4e4516478c18f48a5691124b05aa87c25c915
Samuel Bonini <109481774+samuelbonini@users.noreply.github.com>
2025-05-12 22:27:10 -0300

    att

---
+++ Javascript-Curso/classes/regexp-metodos/script.js
@@ -0,0 +1,31 @@
+// const regexp = /\w+/gi;
+
+// const regexp1 = new RegExp('\\d', 'gi');
+
+// const frase = 'JavaScript Linguagem 101';
+// console.log(frase.replace(regexp1, 'x'))
+
+// const tags = `
+// <ul>
+// <li>Item 1</li>
+// <li>Item 2</li>
+// </ul>
+// `;
+
+// const regexp = /<li/g;
+
+// const resultado = tags.replace(regexp, "div");
+// console.log(resultado);
+
+
+// const emails = `
+// empresa@email.com
+// contato@email.com
+// suporte@email.com
+// `;
+
+// const regexp = /(\w+@)[\w.]+/g
+
+// const resultado = emails.replace(regexp, "$1gmail.com");
+// console.log(resultado)
+
